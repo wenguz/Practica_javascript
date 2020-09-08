17abd5061ee5b7e7d14ef79663fbaa2d92ca00e6
wenguz <wenguz1518@gmail.com>
2020-09-08 13:17:57 -0300

    Instancia otras imagenes
    Se creo las instancias de las imagenes: vaca, cerdo y pollo

--- platzi.js
+++ platzi.js
@@ -3,10 +3,23 @@ var papel = vp.getContext("2d"); //donde se dibujara
 
 //Objeto para crear imagen de fondo
 var fondo =  new Image(); //instancia de una clase
-//carga la fuente de la imagen
+//carga la fuente de la imagen fondo
 fondo.src= "tile.png";
-//evento que permitira dibujar el mapa
+//evento que permitira dibujar el mapa/fondo
 fondo.addEventListener("load",dibujarMapa);
+
+var vaca = new Image();
+vaca.src="vaca.png";
+vaca.addEventListener("load",dibujarVaca);
+
+var cerdo =new Image();
+cerdo.src="cerdo.png";
+cerdo.addEventListener("load",dibujarCerdo);
+
+var pollo= new Image();
+pollo.src="pollo.png";
+pollo.addEventListener("load",dibujarPollo);
+
 
 function dibujarMapa()
 {
