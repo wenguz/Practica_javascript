7a1746e91939cda7b61c1b574631539d338435ec
wenguz <wenguz1518@gmail.com>
2020-09-08 13:13:46 -0300

    Carga de imagen fondo
    Se dibujo la iamgen de fondo en html

--- platzi.js
+++ platzi.js
@@ -1,2 +1,15 @@
 var vp=document.getElementById("villaplatzi"); //llamar al id creado en .html
 var papel = vp.getContext("2d"); //donde se dibujara
+
+//Objeto para crear imagen de fondo
+var fondo =  new Image(); //instancia de una clase
+//carga la fuente de la imagen
+fondo.src= "tile.png";
+//evento que permitira dibujar el mapa
+fondo.addEventListener("load",dibujarMapa);
+
+function dibujarMapa()
+{
+	//agregar imagen al papel
+	papel.drawImage(fondo,0,0); //objeto, coordenadas donde incia
+}
